18dbb7a5298cfdb264b0db055f5077b27bee4d4e
yhj0904 <ujk6073@gmail.com>
2024-03-16 22:12:20 +0900

    member complete!

--- src/main/java/garabu/garabuserver/domain/Category.java
+++ src/main/java/garabu/garabuserver/domain/Category.java
@@ -8,6 +8,8 @@ import lombok.Setter;
 import java.util.ArrayList;
 import java.util.List;
 
+import static jakarta.persistence.FetchType.*;
+
 @Entity
 @Getter @Setter
 public class Category {
@@ -25,11 +27,18 @@ public class Category {
     )
     private List<Item> items = new ArrayList<>();
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "parent_id")
     private Category parent;
 
     @OneToMany(mappedBy = "parent")
     private List<Category> child = new ArrayList<>();
+
+
+    //연관관계 메서드
+    public void addChildCategory(Category child) {
+        this.child.add(child);
+        child.setParent(this);
+    }
 
 }
--- src/main/java/garabu/garabuserver/domain/OrderItem.java
+++ src/main/java/garabu/garabuserver/domain/OrderItem.java
@@ -5,6 +5,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import static jakarta.persistence.FetchType.*;
+
 @Entity
 @Getter @Setter
 public class OrderItem {
@@ -12,11 +14,11 @@ public class OrderItem {
     @Id @GeneratedValue
     @Column(name = "order_item_id")
     private Long id;
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "item_id")
     private Item item;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "order_id")
     private Order order;
 
