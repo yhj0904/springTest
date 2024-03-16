088a2ed0ee57f41f7cc69d1895540bcfe4120623
yhj0904 <ujk6073@gmail.com>
2024-03-16 13:07:53 +0900

    first commit!!!

---
+++ src/main/java/garabu/garabuserver/GarabuserverApplication.java
@@ -0,0 +1,13 @@
+package garabu.garabuserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarabuserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarabuserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/garabu/garabuserver/GarabuserverApplicationTests.java
@@ -0,0 +1,13 @@
+package garabu.garabuserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarabuserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
